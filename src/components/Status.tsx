d69dce63ca691a62bcf0461dec9cd33f8bf5daaa
김보미 <kk3p1q1@gmail.com>
2024-03-04 22:58:26 +0530

    comma util 추가

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 import { status, currentMonth } from "../datas/userData";
+import { comma } from "../utils";
 
 function Status() {
   return (
     <Container>
       <Row>진행중인 월: {currentMonth.getMonth()}월</Row>
       <Row>{status.name}의 스탯</Row>
-      <Row>소지금: {status.money}</Row>
+      <Row>소지금: {comma(status.money)}원</Row>
       <Row>
         개발력: {status.codingSkillPoint}/{status.maxPoint}
       </Row>
